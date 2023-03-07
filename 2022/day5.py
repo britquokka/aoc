1ff3e9ae792462528923e7302cb8749995b4f9f1
britquokka <britquokka@gmail.com>
2023-03-07 22:58:28 +0100

    typo

--- 2022/day5.py
+++ 2022/day5.py
@@ -75,14 +75,14 @@ class Puzzle:
 class TestUtils:
 
     @staticmethod
-    def check_result(test_name: str, expected_result: int, method_to_check, argv):
+    def check_result(test_name: str, expected_result: str, method_to_check, argv):
         current_result = method_to_check(argv)
         assert current_result == expected_result, test_name + ': ' + '(current_result:' + str(
             current_result) + ') != (expected_result:' + str(expected_result) + ')'
         return current_result
 
     @staticmethod
-    def check_result_no_arg(test_name: str, expected_result: int, method_to_check):
+    def check_result_no_arg(test_name: str, expected_result: str, method_to_check):
         current_result = method_to_check()
         assert current_result == expected_result, test_name + ': ' + '(current_result:' + str(
             current_result) + ') != (expected_result:' + str(expected_result) + ')'
