42cd33d93da3c95eea548279a99234a4e06e1b60
britquokka <britquokka@gmail.com>
2023-12-27 12:39:48 +0100

    day14 part 1 clea

--- 2023/day14_part1.py
+++ 2023/day14_part1.py
@@ -1,9 +1,8 @@
-import collections
 import logging
 import os
 import time
 from TestUtils import TestUtils
-from collections import Counter
+
 
 logger = logging.getLogger(__name__)
 
@@ -12,29 +11,35 @@ class Puzzle:
 
     @staticmethod
     def to_platform(file):
-        columns = []
+        platform = []
         with open(file) as f:
-            nb_column = len(f.readline().strip())
-            platform = [['#'] for i in range(nb_column)]
+            nb_column = len(f.readline().strip()) + 2
+            initial_row = ['#'] * nb_column
+            platform.append(initial_row)
             f.seek(0)
             for line in f:
-                row = str(line.strip())
-                for item, column in zip(list(row), platform):
-                    column.append(item)
-            for column in platform:
-                column.append('#')
+                row = '#' + str(line.strip()) + '#'
+                platform.append([c for c in row])
+            platform.append(initial_row.copy())
 
         return platform
+
+    @staticmethod
+    def transpose(matrix: list):
+        result = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+        return result
 
     def __init__(self, file):
         self.platform = self.to_platform(file)
 
     def compute_total_load(self):
+        # transpose row into column
+        platform = self.transpose(self.platform)
         load = 0
-        for column in self.platform:
+        for column in platform:
             column.reverse()
             index_cube_rocks = [i for i, item in enumerate(column) if item == '#']
-            for i in range(0, len(index_cube_rocks)-1, 1):
+            for i in range(0, len(index_cube_rocks) - 1, 1):
                 begin = index_cube_rocks[i]
                 end = index_cube_rocks[i + 1]
                 nb_rounded_rock = column[begin:end].count('O')
@@ -44,7 +49,6 @@ class Puzzle:
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
     logging.basicConfig(level=logging.WARNING)
     TEST_DATA_DIR = os.path.join(os.getcwd(), 'data')
     INPUT_FILE_EXAMPLE = os.path.join(TEST_DATA_DIR, 'day14', 'example.txt')
@@ -67,3 +71,5 @@ if __name__ == '__main__':
     total_load = TestUtils.check_result_no_arg("part1", 109098, puzzle.compute_total_load)
     print("part 1: execution time is ", time.time() - start, " s")
     print("part 1: The total load on the north support beams is  ", total_load)
+
+
